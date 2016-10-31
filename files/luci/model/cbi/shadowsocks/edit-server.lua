f363a320368ee19defceb9c4b63cdedf2908d667
Jian Chang <aa65535@live.com>
2016-10-31 12:30:57 +0800

    fix indentation

--- files/luci/model/cbi/shadowsocks/edit-server.lua
+++ files/luci/model/cbi/shadowsocks/edit-server.lua
@@ -5,38 +5,38 @@ local m, s, o
 local shadowsocks = "shadowsocks"
 local sid = arg[1]
 local encrypt_methods = {
-    "table",
-    "rc4",
-    "rc4-md5",
-    "aes-128-cfb",
-    "aes-192-cfb",
-    "aes-256-cfb",
-    "aes-128-ctr",
-    "aes-192-ctr",
-    "aes-256-ctr",
-    "bf-cfb",
-    "camellia-128-cfb",
-    "camellia-192-cfb",
-    "camellia-256-cfb",
-    "salsa20",
-    "chacha20",
-    "chacha20-ietf",
+	"table",
+	"rc4",
+	"rc4-md5",
+	"aes-128-cfb",
+	"aes-192-cfb",
+	"aes-256-cfb",
+	"aes-128-ctr",
+	"aes-192-ctr",
+	"aes-256-ctr",
+	"bf-cfb",
+	"camellia-128-cfb",
+	"camellia-192-cfb",
+	"camellia-256-cfb",
+	"salsa20",
+	"chacha20",
+	"chacha20-ietf",
 }
 
 local function has_bin(name)
-    return luci.sys.call("command -v %s >/dev/null" %{name}) == 0
+	return luci.sys.call("command -v %s >/dev/null" %{name}) == 0
 end
 
 local function support_fast_open()
-    return luci.sys.exec("cat /proc/sys/net/ipv4/tcp_fastopen 2>/dev/null"):trim() == "3"
+	return luci.sys.exec("cat /proc/sys/net/ipv4/tcp_fastopen 2>/dev/null"):trim() == "3"
 end
 
 m = Map(shadowsocks, "%s - %s" %{translate("ShadowSocks"), translate("Edit Server")})
 m.redirect = luci.dispatcher.build_url("admin/services/shadowsocks/servers-manage")
 
 if m.uci:get(shadowsocks, sid) ~= "servers" then
-    luci.http.redirect(m.redirect)
-    return
+	luci.http.redirect(m.redirect)
+	return
 end
 
 -- [[ Edit Server ]]--
@@ -51,8 +51,8 @@ o = s:option(Flag, "auth", translate("Onetime Authentication"))
 o.rmempty = false
 
 if support_fast_open() and has_bin("ss-local") then
-    o = s:option(Flag, "fast_open", translate("TCP Fast Open"))
-    o.rmempty = false
+	o = s:option(Flag, "fast_open", translate("TCP Fast Open"))
+	o.rmempty = false
 end
 
 o = s:option(Value, "server", translate("Server Address"))
--- files/luci/model/cbi/shadowsocks/servers-manage.lua
+++ files/luci/model/cbi/shadowsocks/servers-manage.lua
@@ -13,32 +13,32 @@ s.addremove = true
 s.template = "cbi/tblsection"
 s.extedit = luci.dispatcher.build_url("admin/services/shadowsocks/edit-server/%s")
 function s.create(...)
-    local sid = TypedSection.create(...)
-    if sid then
-        luci.http.redirect(s.extedit % sid)
-        return
-    end
+	local sid = TypedSection.create(...)
+	if sid then
+		luci.http.redirect(s.extedit % sid)
+		return
+	end
 end
 
 o = s:option(DummyValue, "alias", translate("Alias"))
 function o.cfgvalue(...)
-    return Value.cfgvalue(...) or translate("None")
+	return Value.cfgvalue(...) or translate("None")
 end
 
 o = s:option(DummyValue, "server", translate("Server Address"))
 function o.cfgvalue(...)
-    return Value.cfgvalue(...) or "?"
+	return Value.cfgvalue(...) or "?"
 end
 
 o = s:option(DummyValue, "server_port", translate("Server Port"))
 function o.cfgvalue(...)
-    return Value.cfgvalue(...) or "?"
+	return Value.cfgvalue(...) or "?"
 end
 
 o = s:option(DummyValue, "encrypt_method", translate("Encrypt Method"))
 function o.cfgvalue(...)
-    local v = Value.cfgvalue(...)
-    return v and v:upper() or "?"
+	local v = Value.cfgvalue(...)
+	return v and v:upper() or "?"
 end
 
 return m
