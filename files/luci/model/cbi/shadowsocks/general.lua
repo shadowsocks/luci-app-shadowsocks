f74c185dc9530b8b4f60df8f6395cc94e9e22d3d
Jian Chang <aa65535@live.com>
2017-01-03 17:51:23 +0800

    add start_delay option & close #67

--- files/luci/model/cbi/shadowsocks/general.lua
+++ files/luci/model/cbi/shadowsocks/general.lua
@@ -42,7 +42,7 @@ end)
 m = Map(shadowsocks, "%s - %s" %{translate("ShadowSocks"), translate("General Settings")})
 
 -- [[ Running Status ]]--
-s = m:section(TypedSection, "transparent_proxy", translate("Running Status"))
+s = m:section(TypedSection, "general", translate("Running Status"))
 s.anonymous = true
 
 if has_redir then
@@ -59,6 +59,18 @@ if has_tunnel then
 	o = s:option(DummyValue, "_status", translate("Port Forward"))
 	o.value = get_status("ss-tunnel")
 end
+
+s = m:section(TypedSection, "general", translate("Boot Settings"))
+s.anonymous = true
+
+o = s:option(Value, "start_delay", translate("Start Delay"))
+o:value(0, translate("Unused"))
+for _, v in ipairs({5, 10, 15, 25, 40}) do
+	o:value(v, v .. translate(" seconds"))
+end
+o.datatype = "uinteger"
+o.default = 0
+o.rmempty = false
 
 -- [[ Transparent Proxy ]]--
 if has_redir then
