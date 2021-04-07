4df3442a6285951b06da3ccb70d501bfa4757d46
zmz125000 <zlx220000@msn.com>
2021-04-08 00:36:38 +0800

    merge official 2.0.0

--- files/luci/controller/shadowsocks.lua
+++ files/luci/controller/shadowsocks.lua
@@ -1,4 +1,4 @@
--- Copyright (C) 2014-2017 Jian Chang <aa65535@live.com>
+-- Copyright (C) 2014-2021 Jian Chang <aa65535@live.com>
 -- Licensed to the public under the GNU General Public License v3.
 
 module("luci.controller.shadowsocks", package.seeall)
@@ -8,28 +8,52 @@ function index()
 		return
 	end
 
-	entry({"admin", "services", "shadowsocks"},
+	local page
+
+	page = entry({"admin", "services", "shadowsocks"},
 		alias("admin", "services", "shadowsocks", "general"),
-		_("ShadowSocks"), 10).dependent = true
+		_("ShadowSocks"), 10)
+	page.dependent = true
+	page.acl_depends = { "luci-app-shadowsocks" }
 
-	entry({"admin", "services", "shadowsocks", "general"},
+	page = entry({"admin", "services", "shadowsocks", "general"},
 		cbi("shadowsocks/general"),
-		_("General Settings"), 10).leaf = true
+		_("General Settings"), 10)
+	page.leaf = true
+	page.acl_depends = { "luci-app-shadowsocks" }
 
-	entry({"admin", "services", "shadowsocks", "status"},
-		call("action_status")).leaf = true
+	page = entry({"admin", "services", "shadowsocks", "status"},
+		call("action_status"))
+	page.leaf = true
+	page.acl_depends = { "luci-app-shadowsocks" }
 
-	entry({"admin", "services", "shadowsocks", "servers"},
+	page = entry({"admin", "services", "shadowsocks", "subscription"},
+		cbi("shadowsocks/subscription"),
+		_("Subscription Manage"), 15)
+	page.leaf = true
+	page.acl_depends = { "luci-app-shadowsocks" }
+
+	page = entry({"admin", "services", "shadowsocks", "subscribe"},
+		call("action_subscribe"))
+	page.leaf = true
+	page.acl_depends = { "luci-app-shadowsocks" }
+
+	page = entry({"admin", "services", "shadowsocks", "servers"},
 		arcombine(cbi("shadowsocks/servers"), cbi("shadowsocks/servers-details")),
-		_("Servers Manage"), 20).leaf = true
+		_("Servers Manage"), 20)
+	page.leaf = true
+	page.acl_depends = { "luci-app-shadowsocks" }
 
-	if luci.sys.call("command -v ss-redir >/dev/null") ~= 0 then
+	if luci.sys.call("command -v ss-redir >/dev/null") ~= 0
+		and luci.sys.call("command -v ssr-redir >/dev/null") ~= 0 then
 		return
 	end
 
-	entry({"admin", "services", "shadowsocks", "access-control"},
+	page = entry({"admin", "services", "shadowsocks", "access-control"},
 		cbi("shadowsocks/access-control"),
-		_("Access Control"), 30).leaf = true
+		_("Access Control"), 30)
+	page.leaf = true
+	page.acl_depends = { "luci-app-shadowsocks" }
 end
 
 local function is_running(name)
@@ -39,8 +63,15 @@ end
 function action_status()
 	luci.http.prepare_content("application/json")
 	luci.http.write_json({
-		ss_redir = (is_running("ss-redir") or is_running("v2ray")),
-		ss_local = is_running("ss-local"),
-		ss_tunnel = is_running("ss-tunnel")
+		ss_redir = (is_running("ss-redir") or is_running("ssr-redir")), or is_running("v2ray")),
+		ss_local = (is_running("ss-local") or is_running("ssr-local")),
+		ss_tunnel = (is_running("ss-tunnel") or is_running("ssr-tunnel"))
 	})
+end
+
+function action_subscribe()
+	local section = luci.http.formvalue("section")
+	local code = luci.sys.call("/usr/bin/lua /usr/bin/ss-subscribe %s >/tmp/sub.log" %{section})
+	luci.http.prepare_content("application/json")
+	luci.http.write_json({code = code})
 end
--- files/luci/model/cbi/shadowsocks/servers-details.lua
+++ files/luci/model/cbi/shadowsocks/servers-details.lua
@@ -1,10 +1,13 @@
--- Copyright (C) 2016-2017 Jian Chang <aa65535@live.com>
+-- Copyright (C) 2016-2021 Jian Chang <aa65535@live.com>
 -- Licensed to the public under the GNU General Public License v3.
 
 local m, s, o
 local shadowsocks = "shadowsocks"
 local sid = arg[1]
 local encrypt_methods = {
+	"none",
+	"table",
+	"rc4",
 	"rc4-md5",
 	"aes-128-cfb",
 	"aes-192-cfb",
@@ -26,6 +29,47 @@ local encrypt_methods = {
 	"xchacha20-ietf-poly1305",
 }
 
+local protocols = {
+	"origin",
+	"auth_sha1",
+	"auth_sha1_v2",
+	"auth_sha1_v4",
+	"auth_aes128_md5",
+	"auth_aes128_sha1",
+	"auth_chain_a",
+	"auth_chain_b",
+	"auth_chain_c",
+	"auth_chain_d",
+	"auth_chain_e",
+	"auth_chain_f",
+}
+
+local obfss = {
+	"plain",
+	"http_simple",
+	"http_post",
+	"tls1.2_ticket_auth",
+}
+
+local function has_bin(name)
+	return luci.sys.call("command -v %s >/dev/null" %{name}) == 0
+end
+
+local function has_ss_bins()
+	return has_bin("ss-redir") or has_bin("ss-local") or has_bin("ss-tunnel")
+end
+
+local function has_ssr_bins()
+	return has_bin("ssr-redir") or has_bin("ssr-local") or has_bin("ssr-tunnel")
+end
+
+local function has_v2ray_bin()
+	return has_bin("v2ray") or has_bin("xray")
+
+local function support_fast_open()
+	return luci.sys.exec("cat /proc/sys/net/ipv4/tcp_fastopen 2>/dev/null"):trim() == "3"
+end
+
 m = Map(shadowsocks, "%s - %s" %{translate("ShadowSocks"), translate("Edit Server")})
 m.redirect = luci.dispatcher.build_url("admin/services/shadowsocks/servers")
 m.sid = sid
@@ -44,18 +88,29 @@ s.addremove = false
 o = s:option(Value, "alias", translate("Alias(optional)"))
 o.rmempty = true
 
-o = s:option(Flag, "v2ray", translate("V2Ray"))
+o = s:option(Value, "group", translate("Group Name"))
+o.default = "Default"
 o.rmempty = true
 
-o = s:option(Flag, "fast_open", translate("TCP Fast Open"))
+o = s:option(ListValue, "type", translate("Server Type"))
+if has_ss_bins() then o:value("ss", "Shadowsocks") end
+if has_ssr_bins() then o:value("ssr", "ShadowsocksR") end
+if has_v2ray_bin() then o:value("v2ray", "V2ray") end
+o.default = "ss"
 o.rmempty = false
-o:depends("v2ray","0")
-o:depends("v2ray","")
+
+if support_fast_open() then
+	o = s:option(Flag, "fast_open", translate("TCP Fast Open"))
+	o.rmempty = false
+end
 
 o = s:option(Flag, "no_delay", translate("TCP no-delay"))
+o:depends("type", "ss")
 o.rmempty = false
-o:depends("v2ray","0")
-o:depends("v2ray","")
+
+o = s:option(Value, "host", translate("Server Host"))
+o.datatype = "host"
+o.rmempty = true
 
 o = s:option(Value, "server", translate("Server Address"))
 o.datatype = "ipaddr"
@@ -64,51 +119,64 @@ o.rmempty = false
 o = s:option(Value, "server_port", translate("Server Port"))
 o.datatype = "port"
 o.rmempty = false
-o:depends("v2ray","0")
-o:depends("v2ray","")
+o:depends("type", "ss")
+o:depends("type", "ssr")
 
 o = s:option(Value, "timeout", translate("Connection Timeout"))
 o.datatype = "uinteger"
 o.default = 60
 o.rmempty = false
-o:depends("v2ray","0")
-o:depends("v2ray","")
+o:depends("type", "ss")
+o:depends("type", "ssr")
 
 o = s:option(Value, "password", translate("Password"))
 o.password = true
-o:depends("v2ray","0")
-o:depends("v2ray","")
+o:depends("type", "ss")
+o:depends("type", "ssr")
 
 o = s:option(Value, "key", translate("Directly Key"))
-o:depends("v2ray","0")
-o:depends("v2ray","")
+o:depends("type", "ss")
 
 o = s:option(ListValue, "encrypt_method", translate("Encrypt Method"))
 for _, v in ipairs(encrypt_methods) do o:value(v, v:upper()) end
 o.rmempty = false
-o:depends("v2ray","0")
-o:depends("v2ray","")
+o:depends("type", "ss")
+o:depends("type", "ssr")
 
 o = s:option(Value, "plugin", translate("Plugin Name"))
 o.placeholder = "eg: obfs-local"
-o:depends("v2ray","0")
-o:depends("v2ray","")
+o:depends("type", "ss")
 
 o = s:option(Value, "plugin_opts", translate("Plugin Arguments"))
 o.placeholder = "eg: obfs=http;obfs-host=www.bing.com"
-o:depends("v2ray","0")
-o:depends("v2ray","")
+o:depends("type", "ss")
+
+o = s:option(ListValue, "protocol", translate("Protocol"))
+for _, v in ipairs(protocols) do o:value(v, v:upper()) end
+o:depends("type", "ssr")
+o.rmempty = false
+
+o = s:option(Value, "protocol_param", translate("Protocol Param"))
+o:depends("type", "ssr")
+o.rmempty = true
+
+o = s:option(ListValue, "obfs", translate("Obfuscation"))
+for _, v in ipairs(obfss) do o:value(v, v:upper()) end
+o:depends("type", "ssr")
+o.rmempty = false
+
+o = s:option(Value, "obfs_param", translate("Obfuscation Param"))
+o:depends("type", "ssr")
+o.rmempty = true
 
 o = s:option(Value, "v2ray_path", translate("V2Ray Path(optional)"))
 o.datatype = "or(file, '/dev/null')"
 o.placeholder = "eg: /usr/bin/v2ray/v2ray"
-o:depends("v2ray","1")
+o:depends("type", "v2ray")
 o.rmempty = true
 
 o = s:option(Value, "v2ray_config", translate("V2Ray Config"))
 o.datatype = "or(file, '/dev/null')"
 o.placeholder = "eg: /etc/v2ray/config.json"
-o:depends("v2ray","1")
-o.rmempty = true
-
+o:depends("type", "v2ray")
 return m
